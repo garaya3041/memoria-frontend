b09e6e8324eef5d92b560eab3ec993fdb6cf6fb4
garaya3041 <gabriel.araya@forcast.cl>
2022-03-16 16:42:46 -0300

    fix environment

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -1,3 +1,4 @@
 export const host = {
     url: 'https://murmuring-ridge-38300.herokuapp.com'
+    //url: 'http://127.0.0.1:8000' // Para deploy del server en localhost: Comentar la línea de arriba y descomentar esta línea.
 };
